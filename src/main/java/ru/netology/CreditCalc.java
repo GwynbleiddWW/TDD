85def646689233a368680e8cdd9b0f8225e8dac1
Konstantin Shebalin <championacolironni@gmail.com>
2021-06-14 23:26:28 +0700

    Realization

--- src/main/java/ru/netology/CreditCalc.java
+++ src/main/java/ru/netology/CreditCalc.java
@@ -1,4 +1,23 @@
 package ru.netology;
 
 public class CreditCalc {
+    private double sumOfCredit;
+    private double amountOfMonths;
+    private double interestRate;
+
+    public void paymentOfMonth(double sumOfCredit, double interestRate, double amountOfMonths) {
+        double rate = (interestRate / 100) / 12;
+        double payment = sumOfCredit * ((rate * (Math.pow(1 + rate, amountOfMonths)))
+                / (Math.pow(1 + rate, amountOfMonths) - 1));
+        double totalAmount = payment * amountOfMonths;
+        double overpayment = totalAmount - sumOfCredit;
+
+        double paymentRound = Math.ceil(payment);
+        double totalAmountRound = Math.ceil(totalAmount);
+        double overpaymentRound = Math.ceil(overpayment);
+
+        System.out.println("Месячная сумма платежа: " + paymentRound +
+                "\nОбщая сумма к возрату в банк: " + totalAmountRound +
+                "\nПереплата за весь период: " + overpaymentRound);
+    }
 }
--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,6 +1,22 @@
 package ru.netology;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        double sumOf = 0;
+        double percentOf = 0;
+        double mouthOf = 0;
+        CreditCalc creditCalc = new CreditCalc();
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Кредитный калькулятор.\nВведите сумму кредита: ");
+        sumOf = scan.nextDouble();
+        System.out.println("Введите годовой процент: ");
+        percentOf = scan.nextDouble();
+        System.out.println("Введите количестов месяцев кредита: ");
+        mouthOf = scan.nextDouble();
+
+        creditCalc.paymentOfMonth(sumOf, percentOf, mouthOf);
     }
 }
